a6b0298c5e6bcb090e0420237f4044a9d379efd3
Nguyen Phuoc Duy Khang <duykhangart@gmail.com>
2023-07-14 22:06:18 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,13 @@ import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
-
+import Image from "~/components/Image";
 const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
     <div className={cx("wrapper")}>
-      <img
+      <Image
         className={cx("avatar")}
         src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/f065c5db72bb1d7f8938979f32b73fec~c5_100x100.jpeg?x-expires=1689314400&x-signature=C3XDj56XI9ER26vZozc4PxHIGbc%3D"
         alt="Hoa Vinh"
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,36 +1,27 @@
-import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCircleQuestion,
-  faCircleXmark,
   faCoins,
   faEarthAsia,
   faEllipsisVertical,
   faGear,
   faKeyboard,
   faSignOut,
-  faSpinner,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
 
 import Tippy from "@tippyjs/react/";
-import HeadlessTippy from "@tippyjs/react/headless";
+import "tippy.js/dist/tippy.css";
 
 import Button from "~/components/Button";
-import { Wrapper as PopperWrapper } from "~/components/Popper";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
-import AccountItem from "~/components/AccountItem";
+
 import Menu from "~/components/Popper/Menu";
-import "tippy.js/dist/tippy.css";
-import {
-  MessageIcon,
-  InboxIcon,
-  SearchIcon,
-  UploadIcon,
-} from "~/components/Icons";
+import { MessageIcon, InboxIcon, UploadIcon } from "~/components/Icons";
 import Image from "~/components/Image";
+import Search from "../Search";
 
 const cx = classNames.bind(styles);
 
@@ -66,14 +57,7 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-  const [searchResult, setSearchResult] = useState([]);
   const currentUser = true;
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([]);
-    }, 0);
-  }, []);
 
   //Handle Logic
   const handleMenuChange = (menuItem) => {
@@ -116,33 +100,7 @@ function Header() {
       <div className={cx("inner")}>
         <img src={images.logo} alt="Tik tok" />
 
-        <HeadlessTippy
-          interactive
-          visible={searchResult.length > 0}
-          render={(attrs) => (
-            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
-              <PopperWrapper>
-                <h4 className={cx("search-title")}>Accounts</h4>
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-              </PopperWrapper>
-            </div>
-          )}
-        >
-          <div className={cx("search")}>
-            <input placeholder="Search account and videos" spellCheck={false} />
-            <button className={cx("clear")}>
-              <FontAwesomeIcon icon={faCircleXmark} />
-            </button>
-
-            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
-
-            <button className={cx("search-btn")}>
-              <SearchIcon />
-            </button>
-          </div>
-        </HeadlessTippy>
+        <Search />
 
         <div className={cx("actions")}>
           {currentUser ? (
