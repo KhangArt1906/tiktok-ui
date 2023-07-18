cea6b90c28a1e2cc1995812c9490d5812d02ef63
Nguyen Phuoc Duy Khang <duykhangart@gmail.com>
2023-07-18 10:04:29 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,164 @@
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleQuestion,
+  faCoins,
+  faEarthAsia,
+  faEllipsisVertical,
+  faGear,
+  faKeyboard,
+  faSignOut,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+import Tippy from "@tippyjs/react/";
+import "tippy.js/dist/tippy.css";
+
+import config from "~/config";
+
+import Button from "~/components/Button";
+import styles from "./Header.module.scss";
+import images from "~/assets/images";
+
+import Menu from "~/components/Popper/Menu";
+import { MessageIcon, InboxIcon, UploadIcon } from "~/components/Icons";
+import Image from "~/components/Image";
+import Search from "../Search";
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: "English",
+    children: {
+      title: "Language",
+      data: [
+        {
+          type: "Language",
+          code: "en",
+          title: "English",
+        },
+        {
+          type: "Language",
+          code: "vi",
+          title: "Tieng Viet",
+        },
+      ],
+    },
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: "Feedback and help",
+    to: "/feedback",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: "Keyboard shortcut",
+  },
+];
+
+function Header() {
+  const currentUser = true;
+
+  //Handle Logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case "language":
+        //Handle change language
+
+        break;
+      default:
+    }
+  };
+
+  const userMenu = [
+    {
+      icon: <FontAwesomeIcon icon={faUser} />,
+      title: "View profile",
+      to: "/@hoaaa",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faCoins} />,
+      title: "Get coin",
+      to: "/coin",
+    },
+    {
+      icon: <FontAwesomeIcon icon={faGear} />,
+      title: "Settings",
+      to: "/settings",
+    },
+    ...MENU_ITEMS,
+    {
+      icon: <FontAwesomeIcon icon={faSignOut} />,
+      title: "Log out",
+      to: "/logout",
+      separate: true,
+    },
+  ];
+
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <Link to={config.routes.home} className={cx("logo-link")}>
+          <img src={images.logo} alt="Tik tok" />
+        </Link>
+
+        <Search />
+
+        <div className={cx("actions")}>
+          {currentUser ? (
+            <>
+              {/*Upload Icon */}
+              <Tippy delay={[0, 50]} content="Upload video" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <UploadIcon />
+                </button>
+              </Tippy>
+
+              {/* Message Icon */}
+              <Tippy delay={[0, 50]} content="Message" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <MessageIcon />
+                </button>
+              </Tippy>
+
+              {/* Inbox Icon */}
+              <Tippy delay={[0, 50]} content="Inbox" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <InboxIcon />
+                  <span className={cx("badge")}>19</span>
+                </button>
+              </Tippy>
+            </>
+          ) : (
+            <>
+              <Button text>Upload</Button>
+              <Button primary>Log in</Button>
+            </>
+          )}
+          <Menu
+            items={currentUser ? userMenu : MENU_ITEMS}
+            onChange={handleMenuChange}
+          >
+            {currentUser ? (
+              <Image
+                className={cx("user-avatar")}
+                //src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/f065c5db72bb1d7f8938979f32b73fec~c5_100x100.jpeg?x-expires=1689314400&x-signature=C3XDj56XI9ER26vZozc4PxHIGbc%3D"
+                alt="Nguyen Phuoc Duy Khang"
+                src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
+              />
+            ) : (
+              <button className={cx("more-btn")}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
+          </Menu>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
